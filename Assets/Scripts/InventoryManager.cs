a4beeee9629b8341ac3c9f059dc4296f0045c0a1
dongseon0 <ydsperfect08@ewhain.net>
2026-02-14 17:19:55 +0900

    feat: 씬 구조 정리중(PauseManager.cs)

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -6,6 +6,7 @@ public class InventoryManager : MonoBehaviour
 {
     public GameObject inventoryPanel;
     public TextMeshProUGUI inventoryText;
+    public static InventoryManager Instance; // 중복 생성 안 되게 싱글톤
 
     List<string> items = new List<string>();
 
@@ -28,6 +29,12 @@ public class InventoryManager : MonoBehaviour
     // Scene 넘어가도 인벤토리 유지되도록ㄴ
     void Awake()
     {
+        if (Instance != null)
+        {
+            Destroy(gameObject); // 중복이면 삭제
+            return;
+        }
+        Instance = this;
         DontDestroyOnLoad(gameObject);
     }
 
